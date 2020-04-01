cd3640dd8c115518a4760909746a3736d4361cc9
jonathan <yonibrese@gmail.com>
2020-04-01 18:55:20 +0300

    student models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(models.Institution, InstitutionAdmin)
+
+
+class DegreeTypeAdmin(admin.ModelAdmin):
+    list_display = ['degree']
+
+admin.site.register(models.DegreeType, DegreeTypeAdmin)
+
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ['majorName']
+
+admin.site.register(models.Major, MajorAdmin)
+
+class CountryAdmin(admin.ModelAdmin):
+    lsit_display = ['name']
+
+admin.site.register(models.Country, CountryAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+admin.site.register(models.Student, StudentAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -6,18 +6,43 @@ from django.contrib.auth.models import User
 class Institution(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f'{self.name}'
+    
+
 class DegreeType(models.Model):
     degree = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.degree}'
+    
 
 class Major(models.Model):
     majorName = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f'{self.majorName}'
+    
+
 class Country(models.Model):
-    name = models.CharField(max_lenght=200)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+    
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     degreeType = models.ForeignKey(DegreeType, on_delete=models.CASCADE)
     major = models.ForeignKey(Major, on_delete=models.CASCADE)
+    lookingFor = models.CharField(max_length=50, choices=(
+        ('internship', 'internship'),
+        ('collaborate on a project','collaborate on a project')
+    ))
+    date_avaiable =models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return f'{self.user.lastname},{self.user.firstname}'
+    
 
