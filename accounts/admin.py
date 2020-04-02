9f03bf0ff6779ffa6db924d05aaf6f26bee73d58
jonathan <yonibrese@gmail.com>
2020-04-02 09:45:43 +0300

    company models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from . import models
+
+
 # Register your models here.
 
 class InstitutionAdmin(admin.ModelAdmin):
     list_display = ['name']
+
 
 admin.site.register(models.Institution, InstitutionAdmin)
 
@@ -11,19 +14,40 @@ admin.site.register(models.Institution, InstitutionAdmin)
 class DegreeTypeAdmin(admin.ModelAdmin):
     list_display = ['degree']
 
+
 admin.site.register(models.DegreeType, DegreeTypeAdmin)
+
 
 class MajorAdmin(admin.ModelAdmin):
     list_display = ['majorName']
 
+
 admin.site.register(models.Major, MajorAdmin)
 
+
 class CountryAdmin(admin.ModelAdmin):
-    lsit_display = ['name']
+    list_display = ['name']
+
 
 admin.site.register(models.Country, CountryAdmin)
+
+
+class IndustryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(models.Industry, IndustryAdmin)
+
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['user']
 
+
 admin.site.register(models.Student, StudentAdmin)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['companyName']
+
+
+admin.site.register(models.Company, CompanyAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Institution(models.Model):
@@ -8,41 +9,58 @@ class Institution(models.Model):
 
     def __str__(self):
         return f'{self.name}'
-    
+
 
 class DegreeType(models.Model):
     degree = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.degree}'
-    
+
 
 class Major(models.Model):
     majorName = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.majorName}'
-    
+
 
 class Country(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.name}'
-    
+
+
+class Industry(models.Model):
+    name = models.CharField(max_length=100)
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     degreeType = models.ForeignKey(DegreeType, on_delete=models.CASCADE)
     major = models.ForeignKey(Major, on_delete=models.CASCADE)
-    lookingFor = models.CharField(max_length=50, choices=(
+    lookingFor = models.CharField(max_length=100, choices=(
         ('internship', 'internship'),
-        ('collaborate on a project','collaborate on a project')
+        ('collaborate on a project', 'collaborate on a project')
     ))
-    date_avaiable =models.DateField(auto_now=False, auto_now_add=False)
+    date_avaiable = models.DateField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
         return f'{self.user.lastname},{self.user.firstname}'
-    
 
+
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    companyName = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='company/logo/%Y/%m/%d', null=True, blank=True)
+    num_of_employees = models.IntegerField(null=True, blank=True)
+    lookingTo = models.CharField(max_length=100, choices=(
+        ('hire interns', 'hire interns'),
+        ('hire employees','hire employees'),
+        ('both', 'both')
+    ))
+    industry = models.ForeignKey(Industry, on_delete=models.CASCADE)
+    videoURL = models.URLField(null=True, blank=True)
+    webURL = models.URLField(null=True, blank=True)
